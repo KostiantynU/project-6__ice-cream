1f220dd35680543cc2a084f3687b02fbf08be91d
Ihor Rusyn <igarez20@gmail.com>
2023-01-16 18:32:24 +0200

    replacing not working code
    replacing not working code with code from codepen xD

--- src/js/scrol-up.js
+++ src/js/scrol-up.js
@@ -1,43 +1,41 @@
-// змінна - індикатор факту прокрутки сторінки
-const offset = 100;
-// змінна для звернення до обгортки
-const scrollUp = document.querySelector('.scroll-up');
-// змінна для звернення до path
-const scrollUpSvgPath = document.querySelector('.scroll-up__path');
-// змінна для звернення довжини path
-const pathLength = scrollUpSvgPath.getTotalLength();
+//Scroll back to top
 
-// стилі для довжини path
-scrollUpSvgPath.style.strokeDashrray = `${pathLength} ${pathLength}`;
-// плавна заливка path
-scrollUpSvgPath.style.transition = 'stroke-dashoffset 20ms';
+(function ($) {
+  'use strict';
 
-// змінна яка повертає нам значення прокрутки сторінки по осі Y
-const getTop = () => window.pageYOffset || document.documentElement.scrollTop;
+  $(document).ready(function () {
+    'use strict';
 
-// розрахунок скролу
-const updateDashoffset = () => {
-  const height = document.documentElement.scrollHeight - window.innerHeight;
-  const dashoffset = pathLength - (getTop() * pathLength) / height;
-
-  scrollUpSvgPath.style.strokeDashoffset = dashoffset;
-};
-
-// дії при скролі
-window.addEventListener('scroll', () => {
-  updateDashoffset();
-
-  if (getTop() > offset) {
-    scrollUp.classList.add('scroll-up--active');
-  } else {
-    scrollUp.classList.remove('scroll-up--active');
-  }
-});
-
-// дії при кліці
-scrollUp.addEventListener('click', () => {
-  window.scrollTo({
-    top: 0,
-    behavior: 'smooth',
+    const progressPath = document.querySelector('.progress-wrap path');
+    const pathLength = progressPath.getTotalLength();
+    progressPath.style.transition = progressPath.style.WebkitTransition =
+      'none';
+    progressPath.style.strokeDasharray = pathLength + ' ' + pathLength;
+    progressPath.style.strokeDashoffset = pathLength;
+    progressPath.getBoundingClientRect();
+    progressPath.style.transition = progressPath.style.WebkitTransition =
+      'stroke-dashoffset 10ms linear';
+    const updateProgress = function () {
+      const scroll = $(window).scrollTop();
+      const height = $(document).height() - $(window).height();
+      const progress = pathLength - (scroll * pathLength) / height;
+      progressPath.style.strokeDashoffset = progress;
+    };
+    updateProgress();
+    $(window).scroll(updateProgress);
+    const offset = 50;
+    const duration = 550;
+    jQuery(window).on('scroll', function () {
+      if (jQuery(this).scrollTop() > offset) {
+        jQuery('.progress-wrap').addClass('active-progress');
+      } else {
+        jQuery('.progress-wrap').removeClass('active-progress');
+      }
+    });
+    jQuery('.progress-wrap').on('click', function (event) {
+      event.preventDefault();
+      jQuery('html, body').animate({ scrollTop: 0 }, duration);
+      return false;
+    });
   });
-});
+})(jQuery);
