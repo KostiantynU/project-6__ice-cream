fed24fef76ec139b8171df81956d33f15a5e0c89
Ihor Rusyn <igarez20@gmail.com>
2023-01-16 13:23:01 +0200

    js 90%

--- src/js/scrol-up.js
+++ src/js/scrol-up.js
@@ -1,12 +1,41 @@
-const scrolBtn = document.querySelector('.is-vissible');
-window.onscroll = () => {
-  if (window.scrollY > 700) {
-    scrolBtn.classList.remove('is-vissible--hide');
-  } else if (window.scrollY < 700) {
-    scrolBtn.classList.add('is-vissible--hide');
-  }
+// змінна - індикатор прокрутки
+const offset = 100;
+// змінна для звернення до обгортки
+const scrollUp = document.querySelector('.scroll-up');
+// змінна для звернення до path
+const scrollUpSvgPath = document.querySelector('.scroll-up__path');
+// змінна для звернення довжини path
+const pathLength = scrollUpSvgPath.getTotalLength();
 
-  scrolBtn.onclick = () => {
-    window.scrollTo(0, 0);
-  };
+// стилі для довжини довжини path
+scrollUpSvgPath.style.strokeDashrray = `${pathLength} ${pathLength}`;
+// плавна заливка path
+scrollUpSvgPath.style.transition = 'stroke-dashoffset 20ms';
+
+// змінна яка повертає нам значення прокрутки сторінки по осі Y
+const getTop = () => window.pageYOffset || document.documentElement.scrollTop;
+
+// updateDashoffset
+const updateDashoffset = () => {
+  const height = document.documentElement.scrollHeight - window.innerHeight;
+  const dashoffset = pathLength - (getTop() * pathLength) / height;
+
+  scrollUpSvgPath.style.strokeDashoffset = dashoffset;
 };
+
+// onScroll
+window.addEventListener('scroll', () => {
+  if (getTop() > offset) {
+    scrollUp.classList.add('scroll-up--active');
+  } else {
+    scrollUp.classList.remove('scroll-up--active');
+  }
+});
+
+// click
+scrollUp.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+});
