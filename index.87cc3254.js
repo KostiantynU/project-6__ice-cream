b4fb80f5766e3aa731c311dc52497afc39623a0a
KostiantynU <115901013+KostiantynU@users.noreply.github.com>
2023-01-17 09:09:38 +0000

    Deploying to gh-pages from @ KostiantynU/project-6__ice-cream@ab9e6e5594cd01664389e5fd03b985252b8f1bbd 🚀

--- index.87cc3254.js
+++
@@ -1,2 +0,0 @@
-var scrolBtn=document.querySelector(".is-vissible");window.onscroll=function(){window.scrollY>700?scrolBtn.classList.remove("is-vissible--hide"):window.scrollY<700&&scrolBtn.classList.add("is-vissible--hide"),scrolBtn.onclick=function(){window.scrollTo(0,0)}};
-//# sourceMappingURL=index.87cc3254.js.map
