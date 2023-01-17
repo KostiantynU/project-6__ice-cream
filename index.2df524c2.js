ee9df43b73e7efbac961c7f55e08100abf96027a
KostiantynU <115901013+KostiantynU@users.noreply.github.com>
2023-01-17 08:33:41 +0000

    Deploying to gh-pages from @ KostiantynU/project-6__ice-cream@31ada7455f940b1d2f1e95b4e0549c4b7bde9941 🚀

---
+++ index.2df524c2.js
@@ -0,0 +1,2 @@
+$(".one-time").slick({dots:!0,arrows:!1,dotsClass:"my-dots",infinite:!0,speed:300,slidesToShow:1,adaptiveHeight:!0});
+//# sourceMappingURL=index.2df524c2.js.map
--- index.de2a22cc.js
+++
@@ -1,2 +0,0 @@
-$(".one-time").slick({dots:!0,dotsClass:"my-dots",infinite:!0,speed:300,slidesToShow:1,adaptiveHeight:!0});
-//# sourceMappingURL=index.de2a22cc.js.map
